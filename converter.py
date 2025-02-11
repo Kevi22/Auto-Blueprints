6f1cd71272cea11fa041868ee588b52cd7ece2cc
Max Farnstrom <max.farnstrom@iths.se>
2025-02-11 11:55:32 +0100

    more pngs

--- converter.py
+++ converter.py
@@ -1,7 +1,7 @@
 import fitz  # PyMuPDF, imported as fitz for backward compatibility reasons
-file_path = "data_lake/blueprints/Rosendalsgatan_ny_förskola_totalentreprenad/Upphandlingsdokument/6.3 Beskrivningar/6.3.5 Rambeskrivning El- och telesystem.pdf"
-pages = 76
+file_path = "data_lake/blueprints/Rosendalsgatan_ny_förskola_totalentreprenad/Upphandlingsdokument/6.4 Ritningar/6.4.1 Arkitekt ritningar.pdf"
+pages = 21
 doc = fitz.open(file_path)  # open document
 page = doc[pages]
 pix = page.get_pixmap()  # render page to an image
-pix.save(f"Blåsväder10_{pages}.png")
+pix.save(f"Rosen13_{pages}.png")
