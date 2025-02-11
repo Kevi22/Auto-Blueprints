95c2648595a38f4ea3655fc87dd3e2288c443a9f
Max Farnstrom <max.farnstrom@iths.se>
2025-02-11 13:49:40 +0100

    Last images

--- converter.py
+++ converter.py
@@ -1,7 +1,7 @@
 import fitz  # PyMuPDF, imported as fitz for backward compatibility reasons
 file_path = "data_lake/blueprints/Rosendalsgatan/Upphandlingsdokument/6.4 Ritningar/6.4.4 Akustik ritningar.pdf"
-pages = 1
+pages = 9
 doc = fitz.open(file_path)  # open document
 page = doc[pages]
 pix = page.get_pixmap()  # render page to an image
-pix.save(f"Rosen14_{pages}.png")
+pix.save(f"Rosen21_{pages}.png")
