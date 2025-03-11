df8c8c9d9cd5d2aa2730ac9074ac83bc3a01cea5
Rithwan Mohammed <ardyG81@hotmail.com>
2025-03-11 14:42:39 +0100

    made some changes

--- converter.py
+++ converter.py
@@ -1,7 +1,7 @@
 import fitz  # PyMuPDF, imported as fitz for backward compatibility reasons
-file_path = "data_lake/blueprints/Jenningsskolan/bilagor/12_Ritningar/12.2 Arkitektriningar enligt förteckning/A-43-6-001.pdf"
+file_path = "data_lake/blueprints/Rosendalsgatan/6.4 Ritningar/6.4.1 Arkitekt ritningar.pdf"
 pages = 0
 doc = fitz.open(file_path)  # open document
 page = doc[pages]
 pix = page.get_pixmap()  # render page to an image
-pix.save(f"A-43-6-001{pages}.png")
+pix.save(f"Rosendals_0{pages}.png")
